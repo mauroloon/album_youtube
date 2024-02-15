f8890730bea91136dec532c0ff885ef48053709b
Mauroloon <mauricio.quezadagarcia@gmail.com>
2024-02-15 12:14:21 -0300

    test
    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 import './assets/main.css'
-import './bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
 
-import './bootstrap/dist/js/bootstrap.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
