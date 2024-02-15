59d105d0bccff912a42a742ff6435e87f4afe6a9
Mauroloon <mauricio.quezadagarcia@gmail.com>
2024-02-15 11:41:52 -0300

    Update frontend
    Se actualiza el front, se completan funcionalidaes, falta mejorar el diseño.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,7 +16,7 @@
         <button @click="addLink" class="btn btn-primary mb-3">Añadir</button>
       </div>
     </div>
-    <Link :links="registerLinks" />
+    <Link :links="registerLinks" @delete-link="deleteLink" />
   </div>
 </template>
 
@@ -29,18 +29,17 @@ const linkVideo = ref("");
 const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
 
 const apiKeyYoutube = import.meta.env.VITE_API_KEY_YOUTUBE;
-const urlApiGateway = import.meta.env.VITE_URL_API_GATEWAY
-const urlApiGoogle = import.meta.env.VITE_URL_API_GOOGLE
+const urlApiGateway = import.meta.env.VITE_URL_API_GATEWAY;
+const urlApiGoogle = import.meta.env.VITE_URL_API_GOOGLE;
 
 onMounted(() => {
-  //loadLinks()
+  loadLinks();
 });
 
 const loadLinks = async () => {
-  const response = await fetch(urlApiGateway+'/links');
+  const response = await fetch(urlApiGateway + "/links");
   const data = await response.json();
-  console.log(data);
-  // TODO: insertar datos en variable registerLinks
+  registerLinks.value = data.Items;
 };
 
 const addLink = async () => {
@@ -48,25 +47,48 @@ const addLink = async () => {
   if (match && match[1]) {
     const id = match[1];
 
-    const response_youtube = await fetch(`${urlApiGoogle}/videos?part=snippet&id=${id}&key=${apiKeyYoutube}`);
+    const response_youtube = await fetch(
+      `${urlApiGoogle}/videos?part=snippet&id=${id}&key=${apiKeyYoutube}`
+    );
     const data_youtube = await response_youtube.json();
 
     const data_link = data_youtube.items[0].snippet;
-    const title = data_link.title
-    const description = data_link.description
-    const thumbnails = data_link.thumbnails
+    const title = data_link.title;
+    const description = data_link.description;
+    const thumbnails = data_link.thumbnails;
 
     const params = {
-      'idLink': id,
-      'title': title,
-      'description': description,
-      'thumbnails': thumbnails
-    }
-    // TODO: Mandar datos a apigateway mediante post
-    console.log(params)
+      idLink: id,
+      title: title,
+      description: description,
+      thumbnails: thumbnails,
+    };
 
+    // TODO: Mandar datos a apigateway mediante post
+    const response_add = await fetch(urlApiGateway + "/link", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(params),
+    });
+
+    // TODO: agregar mensaje de éxito y agregar nuevo video al array (o cargarlos nuevamente (menos optimo))
+    if (response_add.status == 200) {
+      registerLinks.value.push(params);
+    }
   } else {
     console.log("No se pudo extraer el ID del video.");
+  }
+};
+
+const deleteLink = async (id) => {
+  const response_remove = await fetch(`${urlApiGateway}/link/${id}`, {
+    method: "DELETE",
+  });
+
+  if (response_remove.status == 200) {
+    registerLinks.value = registerLinks.value.filter((item) => item.id !== id);
   }
 };
 </script>
--- frontend/src/components/Link.vue
+++ frontend/src/components/Link.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="row">
-    <div class="col-4" v-for="item in links" :index="item.id" >
+    <div class="col-4" v-for="item in links" :index="item.id">
       <img
-        :src="item.src"
+        :src="item.thumbnails.standard.url"
         class="rounded"
         height="200"
         width="200"
+        @click="openModal(item)"
       />
+      <br>
+      <button type="button" class="btn btn-danger" @click="emit('delete-link', item.id)">Eliminar</button>
     </div>
   </div>
+  <ModalComponent :isOpen="isModalOpened" @modal-close="closeModal" name="first-modal">
+    <!-- <template #header>Custom header</template> -->
+    <template #content>
+      <div class="row">
+        <div class="col">
+          <img 
+          :src="linkSelectedData.src"
+          class="rounded"
+          height="200"
+          width="200"
+          />
+        </div>
+        <div class="col">
+          <h3> {{ linkSelectedData.title }} </h3>
+          <br>
+          <p> {{ linkSelectedData.description }}</p>
+        </div>
+      </div>
+    </template>
+    <!-- <template #footer>Custom content</template> -->
+  </ModalComponent>
 </template>
 
 <script setup>
-  const props = defineProps({
-    links: Array
-  })
+import ModalComponent from "./Modal.vue";
+import { ref } from "vue";
+
+const isModalOpened = ref(false);
+const linkSelectedData = ref(null)
+const emit = defineEmits(["delete-link"]);
+
+const props = defineProps({
+  links: Array,
+});
+
+const openModal = (item) => {
+  isModalOpened.value = true;
+  linkSelectedData.value = {
+    'title': item.title,
+    'description': item.description,
+    'src': item.thumbnails.standard.url,
+  }
+};
+const closeModal = () => {
+  isModalOpened.value = false;
+  linkSelectedData.value = null;
+};
 </script>
